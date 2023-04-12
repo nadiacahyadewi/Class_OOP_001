d879ced707c6bf2e8cbdde4b7a67e748cb60200c
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-04-12 14:33:41 +0700

    Add project files.

---
+++ oopkelasA/oopkelasA.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+
+private:
+	int nim;
+	string nama;
+public :
+	void inputdata() {
+		cout << "Masukkan NIM : ";
+		cin >> nim;
+		cout << "Masukkan Nama : ";
+		cin >> nama;
+	}
+	void tampilandata() {
+		cout << "\nNIM : " << nim << endl;
+		cout << "\nNama : " << nama << endl;
+	}
+};
